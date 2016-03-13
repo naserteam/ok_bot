8489ec9b9f74a3b908c277a82f3a1012ae0f66fa
shayanchabok007 <shayanchabok5@gmail.com>
2016-03-13 21:06:30 +0430

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,29 @@
+do
+
+function run(msg, matches)
+  return '✅I R A N P O W E R v2✅'.. VERSION .. [[ 
+ 
+  Developer : @shayan31323
+  
+  Channel : @iranpower_ch
+  
+ I R A N P O W E R🌏 V2
+  
+  All rights reserved.
+  __________________]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "version: Shows bot version",
+  patterns = {
+    "^[Vv]ersion$"
+  }, 
+  run = run 
+}
+
+end
+--Copyright and edit; @behroozyaghi
+--Persian Translate; @behroozyaghi
+--ch : @nod32team
+--کپی بدون ذکر منبع حرام است
